715362a1cb684bc6b68e885614cc77efcc8f3b57
ZZX4R0 <manisorn0803@gmail.com>
2025-01-31 09:08:00 +0000

    complete

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -3,10 +3,12 @@
 using namespace std;
 
 int main(){
-	int data[] = {7,8,2,1,2,4,6,9,0,1}; 
+	int data[] = {7,8,2,1,2,4,6,9,0,1};
+	int size = sizeof(data) / sizeof(data[0]);
 	cout << "Before: ";
 	for(int i = 0; i<10; i++) cout << data[i] << " ";
-	sort(&data[0],&data[10]);
+	sort(data, data + 5);
+	sort(data + 5, data + size);
 	cout << "\nAfter: ";
 	for(int i = 0; i<10; i++) cout << data[i] << " ";
 	return 0;
